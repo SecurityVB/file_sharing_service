b64d6b2ea8c06a73bfab0307c9d376d9def24164
SecurityVB <koworking9992@gmail.com>
2026-09-15 00:36:22 +0300

    fix upload files

--- src/files/config.py
+++ src/files/config.py
@@ -1,3 +1,3 @@
 from src.config import BASE_DIR
 
-UFILES_DIR = BASE_DIR + "src/files/ufiles"
+UFILES_DIR = BASE_DIR / "src" / "files" / "ufiles"
--- src/files/router.py
+++ src/files/router.py
@@ -8,15 +8,17 @@ from src.files.config import UFILES_DIR
 
 FilesRouter = APIRouter()
 
-@FilesRouter.post("files/upload")
+@FilesRouter.post("/files/upload")
 async def upload_file(ufiles: list[UploadFile]):
     id_dir = hashlib.md5(str(time.time()).encode("utf-8")).hexdigest()
+    directory = UFILES_DIR / id_dir
     for ufile in ufiles:
         file = ufile.file
         filename = ufile.filename
-        with open(UFILES_DIR + id_dir + filename, 'wb') as f:
+        directory.mkdir(parents=True, exist_ok=True)
+        with open(directory / filename, 'wb') as f:
             f.write(file.read())
 
-@FilesRouter.post("files/download/{id}")
+@FilesRouter.post("/files/download/{id}")
 async def upload_file(id: str):
     pass

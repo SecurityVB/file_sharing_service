930b459ffa7f204d4981e457348e6001763b938c
SecurityVB <koworking9992@gmail.com>
2026-09-21 01:03:57 +0300

    file sharing

--- src/files/router.py
+++ src/files/router.py
@@ -1,7 +1,10 @@
 import hashlib
+import shutil
 import time
+from zipfile import ZipFile, ZIP_DEFLATED
 
 from fastapi import APIRouter, UploadFile
+from starlette.responses import FileResponse
 
 from src.files.config import UFILES_DIR
 
@@ -10,15 +13,34 @@ FilesRouter = APIRouter()
 
 @FilesRouter.post("/files/upload")
 async def upload_file(ufiles: list[UploadFile]):
-    id_dir = hashlib.md5(str(time.time()).encode("utf-8")).hexdigest()
-    directory = UFILES_DIR / id_dir
-    for ufile in ufiles:
-        file = ufile.file
-        filename = ufile.filename
+    id_for_files = hashlib.md5(str(time.time()).encode("utf-8")).hexdigest()
+    directory = UFILES_DIR / id_for_files
+    if len(ufiles) <= 1:
+        for ufile in ufiles:
+            file = ufile.file
+            filename = ufile.filename
+            directory.mkdir(parents=True, exist_ok=True)
+            with open(directory / filename, 'wb') as f:
+                f.write(file.read())
+    else:
         directory.mkdir(parents=True, exist_ok=True)
-        with open(directory / filename, 'wb') as f:
-            f.write(file.read())
+        with ZipFile(directory / f"{id_for_files}.zip", "w", ZIP_DEFLATED) as archive:
+            for ufile in ufiles:
+                filename = ufile.filename
 
-@FilesRouter.post("/files/download/{id}")
-async def upload_file(id: str):
-    pass
+                with archive.open(filename, "w") as archive_file:
+                    shutil.copyfileobj(ufile.file, archive_file)
+
+    return {
+        "status": True,
+        "id": id_for_files,
+    }
+
+@FilesRouter.get("/files/download/{id}")
+async def get_files(id: str):
+    directory = UFILES_DIR / id
+    file_path = next(directory.iterdir())
+    return FileResponse(
+        path=file_path,
+        filename=file_path.name
+    )
